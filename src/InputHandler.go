bedfdcf67bbfd46707d5ede90664013638569165
Moonawar_Laptop <moonawar@protonmail.com>
2023-02-27 00:32:23 +0700

    adding input handler

--- src/InputHandler.go
+++ src/InputHandler.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+)
+
+func readPoints() []Point {
+	var points []Point
+	var n int
+
+
+	fmt.Printf("\nEnter number of points : ")
+	fmt.Scan(&n)
+
+	fmt.Printf("\nFor each point, write each coordinates seperated by a space\nExample : 1 2 3\n\n")
+	for i := 0; i < n; i++ {
+		var x, y, z int
+		fmt.Printf("Point %d : ", i+1)
+		fmt.Scan(&x, &y, &z)
+		points = append(points, *NewPoint(x, y, z))
+	}
+
+	return points
+}
+
+func printPoints (points []Point) {
+	for i, point := range points {
+		fmt.Printf("Point %d : (%d, %d, %d)\n", i, point.getX(), point.getY(), point.getZ())
+	}
+}
+
+func main() {
+	points := readPoints()
+	
+	// printPoints(points)
+}	
