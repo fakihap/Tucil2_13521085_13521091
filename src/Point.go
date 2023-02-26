43262c05b867c76b927035a54cf4964bb32804bb
Moonawar_Laptop <moonawar@protonmail.com>
2023-02-27 02:04:40 +0700

    point: more generalized form

--- src/Point.go
+++ src/Point.go
@@ -6,46 +6,18 @@ var (
 
 type Point struct {
 	ID int
-	x int
-	y int
-	z int
+	dimension int
+	val []int
 }
 
 // Constructor
-func NewPoint(_x int, _y int, _z int) *Point {
+func NewPoint(dimension int, val []int) *Point {
 	p := new(Point)
-	p.x = _x
-	p.y = _y
-	p.z = _z
+	p.dimension = dimension
+	p.val = val
 
 	num_of_points++
 	p.ID = num_of_points
 
 	return p
-}
-
-// Getter
-func (p Point) getX() int {
-	return p.x
-}
-
-func (p Point) getY() int {
-	return p.y
-}
-
-func (p Point) getZ() int {
-	return p.z
-}
-
-// Setter
-func (p * Point) setX(newX int) {
-	p.x = newX;
-}
-
-func (p * Point) setY(newY int) {
-	p.y = newY;
-}
-
-func (p * Point) setZ(newZ int) {
-	p.z = newZ;
 }
