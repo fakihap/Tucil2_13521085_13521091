5d5ef067440f5ece102d7c066c7b896b52ff3234
fakihap <fakih.anugerah@gmail.com>
2023-02-27 09:16:59 +0700

    feat : Solver "class" and sort func

--- CPairSolver/Solver.go
+++ CPairSolver/Solver.go
@@ -2,10 +2,57 @@ package CPairSolver
 
 import "fmt"
 
-func sort(pos []float64) {
-
+type Solver struct {
+	points    []Point
+	dimension int
 }
 
-func Zolve() {
-	fmt.Print("adasd")
+func NewSolver(points ...Point) *Solver {
+	s := new(Solver)
+
+	s.points = points
+	s.dimension = len(points)
+
+	return s
+}
+
+func getPartition(slice []Point, a, b, axis int) int {
+	curPiv := slice[b].GetAxisValue(axis)
+
+	i := a - 1
+
+	for j := a; j < b; j++ {
+		if slice[j].GetAxisValue(axis) <= curPiv {
+			i = i + 1
+
+			slice[i], slice[j] = slice[j], slice[i]
+		}
+	}
+
+	slice[i+1], slice[b] = slice[b], slice[i+1]
+
+	return i + 1
+}
+
+func (s Solver) quickSort(a, b, axis int) {
+	if a < b {
+		p := getPartition(s.points, a, b, axis)
+
+		s.quickSort(a, p-1, axis)
+		s.quickSort(p+1, b, axis)
+	}
+}
+
+func (s Solver) Sort(axis int) {
+	s.quickSort(0, len(s.points)-1, axis)
+}
+
+func (s Solver) Print() {
+	for _, point := range s.points {
+		for _, val := range point.val {
+			fmt.Print(val, " ")
+		}
+
+		fmt.Println()
+	}
 }
--- main.go
+++ main.go
@@ -10,7 +10,19 @@ func main() {
 
 	// fmt.Println(p.GetX())
 	// fmt.Println(p.GetY())
-	p := CPairSolver.NewPoint(3, []int{1, 2, 3})
+	p1 := CPairSolver.NewPoint(1, 2, 3)
 
-	fmt.Println(p.GetAxisValue(0))
+	//fmt.Println(p.GetAxisValue(0))
+	p2 := CPairSolver.NewPoint(2, 0, 0)
+	p3 := CPairSolver.NewPoint(-1, 999, 999)
+
+	s := CPairSolver.NewSolver(*p1, *p2, *p3)
+
+	fmt.Println("before")
+	s.Print()
+
+	s.Sort(0)
+
+	fmt.Println("after")
+	s.Print()
 }
